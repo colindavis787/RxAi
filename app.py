a84bda21ce1f29162d8de50eb8ba2fd4c9a12acd
Colin Davis <colindavis787@gmail.com>
2025-05-30 22:04:27 -0400

    Fix DeprecationError by removing pre-authorized parameter in Authenticate

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['pre-authorized']
+    config['cookie']['expiry_days']
 )
 
 # Authentication
